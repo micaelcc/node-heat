a13cdbb6c2603d14772ed0931063996f4afa814d
Micael Lima <micaelcomp@alu.ufc.br>
2021-10-23 22:33:16 -0300

    feat: added login github route redirect

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
+import "dotenv/config";
+
 import express from 'express';
 
 const app = express();
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+});
 
 app.listen(4000, () => console.log(
   "Server is run on port 4000!"
